8b2738e243576d052295585707541d76089cd701
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2014-10-06 09:42:39 +0200

    Create core bundle for rest api and core functionality
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

---
+++ bundles/com.runolarity.p2.server.core/README.md
@@ -0,0 +1,4 @@
+p2 Server Core
+==============
+
+This is the core bundle for the p2 server. It contains the core functionality developed entirely in Java and hosts the REST API as Gyrex Application.
---
+++ bundles/com.runolarity.p2.server.core/src/com/runolarity/p2/server/core/rest/api/ApiRootResource.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2014 <company> and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     <author> - initial API and implementation
+ */
+package com.runolarity.p2.server.core.rest.api;
+
+import javax.ws.rs.Path;
+
+/**
+ * Root of the p2 Server API
+ */
+@Path("/")
+public class ApiRootResource {
+
+}
